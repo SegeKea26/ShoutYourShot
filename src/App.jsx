0037fe67358271d501778f9d0f1c29a72b989bf9
SegeKea26 <keano.segers@student.arteveldehs.be>
2026-01-21 20:51:41 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router'
+import SetupPage from './pages/SetupPage/SetupPage'
+import GamePage from './pages/GamePage/GamePage'
+import StatisticsPage from './pages/StatisticsPage/StatisticsPage'
+
 import './App.css'
 
 function App() {
-
   return (
     <>
-
+      <Routes>
+        <Route path="/" element={<SetupPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/stats" element={<StatisticsPage />} />
+      </Routes>
     </>
   )
 }

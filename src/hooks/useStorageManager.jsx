6c099c3034c706edb56697947a58121a8ca56adc
SegeKea26 <keano.segers@student.arteveldehs.be>
2026-01-21 22:58:32 +0100

    added storage logic hook

---
+++ src/hooks/useStorageManager.jsx
@@ -0,0 +1,12 @@
+export function newStorage() {
+    const defaultStorage = { setup: {}, game: {} };
+
+    localStorage.setItem('ShoutYourShot', JSON.stringify(defaultStorage));
+}
+
+export function addSetupToStorage(name, val) {
+    const storage = JSON.parse(localStorage.getItem('ShoutYourShot'));
+
+    storage.setup[name] = val;
+    localStorage.setItem('ShoutYourShot', JSON.stringify(storage))
+}

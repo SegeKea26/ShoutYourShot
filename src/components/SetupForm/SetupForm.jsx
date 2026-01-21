86f946a38226a1769cf7f462e90e9a48e5445b35
SegeKea26 <keano.segers@student.arteveldehs.be>
2026-01-21 22:59:10 +0100

    updated setupForm

--- src/components/SetupForm/SetupForm.jsx
+++ src/components/SetupForm/SetupForm.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import { addSetupToStorage } from '../../hooks/useStorageManager'
 
-function SetupForm({ title, subtitle, children }) {
+function SetupForm({ title, subtitle, children, onNext, prevDisabled, onPrev }) {
 
     if (!title) {
         console.error("please, enter the title for your form")
         title = "unknown";
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+
+        for (const [name, value] of formData.entries()) {
+            addSetupToStorage(name, value);
+        }
+
+        if (onNext) {
+            onNext();
+        }
+    }
+
     return (
-        <div className="setup-form">
+        <form className="setup-form" onSubmit={handleSubmit}>
             <div className="setup-form__header">
                 <h2 className="setup-form__title">{title}</h2>
                 {subtitle &&
@@ -19,7 +33,12 @@ function SetupForm({ title, subtitle, children }) {
             <div className="setup-form__body">
                 {children}
             </div>
-        </div>
+
+            <div>
+                <button type='button' disabled={prevDisabled} onClick={onPrev}>prev</button>
+                <button type="submit">Next</button>
+            </div>
+        </form>
     )
 }
 

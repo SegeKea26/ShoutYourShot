84043f85cbb293062e22f3d923beb0114d3bc36d
SegeKea26 <keano.segers@student.arteveldehs.be>
2026-01-22 14:13:08 +0100

    updated SetupOption with custom input logic added remembers previously selected option

--- src/components/OptionButton/SetupOption.jsx
+++ src/components/OptionButton/SetupOption.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getSetupFromStorage } from '../../managers/useStorageManager';
 
-function SetupOption({ label, name, val }) {
+function previouslyChecked(name, val, def = false) {
+    const setup = getSetupFromStorage();
 
-    return (
-        <label className='setup-option'>
-            <span className='setup-option__text'>{label}</span>
-            <input className='setup-option__input' type='radio' name={name} value={val} />
-        </label>
-    )
+    if (!setup || !(name in setup)) return def;
+
+    return String(setup[name]) === String(val);
 }
 
-export default SetupOption
+function SetupOption({ label, name, val, def = false, hasTextField = false }) {
+    const setup = getSetupFromStorage();
+
+    const initialVal = setup && (name in setup) ? String(setup[name]) : '1';
+    
+    const [currentVal, setCurrentVal] = useState(initialVal);
+
+    if (hasTextField) {
+        return (
+            <label className="setup-option">
+                <span className="setup-option__text">{label}</span>
+                <input
+                    className="setup-option__input"
+                    type="radio"
+                    name={name}
+                    value={currentVal}
+                    defaultChecked={previouslyChecked(name, currentVal, def)}
+                />
+                <input
+                    type="number"
+                    defaultValue={initialVal}
+                    min="1"
+                    step="2"
+                    onChange={(e) => setCurrentVal(String(e.target.value))}
+                />
+            </label>
+        );
+    }
+
+    return (
+        <label className="setup-option">
+            <span className="setup-option__text">{label}</span>
+            <input
+                className="setup-option__input"
+                type="radio"
+                name={name}
+                value={val}
+                defaultChecked={previouslyChecked(name, val, def)}
+            />
+        </label>
+    );
+}
+
+export default SetupOption;

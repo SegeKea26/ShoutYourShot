2367f9c351b1135aac21ca2da4e12b5a7ae584ee
SegeKea26 <keano.segers@student.arteveldehs.be>
2026-01-26 21:43:27 +0100

    refactored SetupForm component

--- src/components/SetupForm/SetupForm.jsx
+++ src/components/SetupForm/SetupForm.jsx
@@ -1,48 +1,8 @@
-import React, { useState, useRef } from 'react'
-
-import { addSetupToStorage } from '../../managers/useStorageManager'
-import { validateName } from '../../managers/useValidationManager'
+import React from 'react'
+import useSetupForm from '../../hooks/useSetupForm'
 
 function SetupForm({ title, subtitle, children, onNext, prevDisabled, onPrev, submitLabel = 'Next' }) {
-
-    const [errors, setErrors] = useState({});
-    const firstErrorRef = useRef(null);
-
-    if (!title) {
-        console.error("please, enter the title for your form")
-        title = "unknown";
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-
-        const entries = Array.from(formData.entries());
-        const newErrors = {};
-
-        for (const [name, value] of entries) {
-            if (/^player/i.test(name)) {
-                const err = validateName(value);
-                if (err) newErrors[name] = err;
-            }
-        }
-
-        if (Object.keys(newErrors).length > 0) {
-            setErrors(newErrors);
-            firstErrorRef.current = Object.keys(newErrors)[0];
-            return;
-        }
-
-        setErrors({});
-
-        for (const [name, value] of entries) {
-            addSetupToStorage(name, value);
-        }
-
-        if (onNext) {
-            onNext();
-        }
-    }
+    const { errors, handleSubmit } = useSetupForm({ onNext })
 
     return (
         <form className="setup-form" onSubmit={handleSubmit}>

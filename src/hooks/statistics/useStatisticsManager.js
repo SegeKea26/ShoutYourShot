78c6cfd9b9d4bef1a398a080d9e83e9105eeb634
SegeKea26 <keano.segers@student.arteveldehs.be>
2026-01-27 14:23:54 +0100

    added game logic

---
+++ src/hooks/statistics/useStatisticsManager.js
@@ -0,0 +1,7 @@
+import { computeGameStatistics } from './computeGameStatistics'
+import { computeLegStatistics } from './computeLegStatistics'
+import { computePlayerStatistics } from './computePlayerStatistics'
+
+export { computeGameStatistics, computeLegStatistics, computePlayerStatistics }
+
+export default { computeGameStatistics, computeLegStatistics, computePlayerStatistics }
